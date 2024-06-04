a58ce00116da48dc416790a66da94682aa21cadc
tqhit2001 <thieuqhuy2001@gmail.com>
2024-06-04 16:53:31 +0700

    + add animation open/close to container counter

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ContainerCounter : BaseCounter
 {
+    public event EventHandler OnPlayerGrabbedObject;
+
     [SerializeField] private KitchenObjectSO _kitchenObjectSO;
 
     public override void Interact(Player player)
     {
         Transform kitchenObjectTransform = Instantiate(_kitchenObjectSO.prefab);
         kitchenObjectTransform.GetComponent<KitchenObject>().KitchenObjectParent = player;
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
     }
 }
